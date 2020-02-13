b8a9d94a1c09174dd4ef4d0a559e832388f87064
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-12 22:48:34 -0700

    Add prepare method

--- src/sqlite.zig
+++ src/sqlite.zig
@@ -25,6 +25,34 @@ pub const SQLite = struct {
             .db = dbNonNull,
         };
     }
+
+    pub fn close(self: *const @This()) void {
+        sqliteAssertOkay(sqlite3_close(self.db));
+    }
+
+    pub fn prepare(self: *const @This(), sql: [:0]const u8, sqlTail: ?*[:0]const u8) !?SQLiteStmt {
+        var stmt: ?*sqlite3_stmt = undefined;
+        const sqlLen = @intCast(c_int, sql.len);
+        var tail: ?[*]u8 = undefined;
+
+        var rc = sqlite3_prepare_v2(self.db, sql, sqlLen, &stmt, &tail);
+
+        if (rc != SQLITE_OK) {
+            return error.PrepareFailed;
+        }
+
+        return SQLiteStmt{
+            .stmt = stmt orelse return null,
+        };
+    }
+};
+
+pub const SQLiteStmt = struct {
+    stmt: *sqlite3_stmt,
+
+    pub fn finalize(self: *const SQLiteStmt) void {
+        sqliteAssertOkay(sqlite3_finalize(self.stmt));
+    }
 };
 
 fn sqliteAssertOkay(sqlite_rc: c_int) void {
@@ -34,5 +62,15 @@ fn sqliteAssertOkay(sqlite_rc: c_int) void {
 }
 
 test "open in memory sqlite db" {
-    const db = SQLite.open(":memory:");
+    const db = try SQLite.open(":memory:");
+    defer db.close();
+
+    const sqlCreateTable = "CREATE TABLE IF NOT EXISTS hello (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);";
+    const sql =
+        \\ INSERT INTO hello (name) VALUES ("world"), ("foo");
+        \\ SELECT * FROM hello;
+    ;
+
+    const create_stmt = (try db.prepare(sqlCreateTable, null)) orelse return error.NullCreateStmt;
+    defer create_stmt.finalize();
 }
