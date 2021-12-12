d31d0bfafa8f148822a3da46f76bcc3e2966e308
LeRoyce Pearson <me@leroycepearson.dev>
2021-12-11 19:42:28 -0700

    Add sqlite3_config interface

--- src/sqlite3.zig
+++ src/sqlite3.zig
@@ -333,6 +333,233 @@ pub const realloc64 = sqlite3_realloc64;
 pub const free = sqlite3_free;
 pub const msize = sqlite3_msize;
 
+// Config
+pub extern fn sqlite3_config(c_int, ...) c_int;
+
+pub fn config(params: ConfigParams) !void {
+    const option = @enumToInt(params);
+    const result = switch (params) {
+        .singlethread,
+        .multithread,
+        .serialized,
+        => sqlite3_config(option),
+
+        .malloc,
+        .getmalloc,
+        => |p| sqlite3_config(option, p),
+
+        .small_malloc,
+        .memstatus,
+        .uri,
+        .covering_index_scan,
+        => |p| sqlite3_config(option, @boolToInt(p)),
+
+        .pagecache => |p| sqlite3_config(option, p.pMem, p.sz, p.n),
+        .heap => |p| sqlite3_config(
+            option,
+            if (p.mem) |m| m.ptr else null,
+            if (p.mem) |m| m.len else 0,
+        ),
+
+        .mutex,
+        .getmutex,
+        => |p| sqlite3_config(option, p),
+
+        .lookaside => |p| sqlite3_config(option, p.defaultSlotSize, p.defaultSlotsPerConnection),
+
+        .pcache2,
+        .getpcache2,
+        => |p| sqlite3_config(option, p),
+
+        .log => |p| sqlite3_config(option, p.logFn, p.userdata),
+        .sqllog => |p| sqlite3_config(option, p.logFn, p.userdata),
+        .mmap_size => |p| sqlite3_config(option, p.defaultSizeLimit, p.maximumSizeLimit),
+        .win32_heapsize => |p| sqlite3_config(option, p),
+        .pcache_hdrsz => |p| sqlite3_config(option, p),
+        .pmasz => |p| sqlite3_config(option, p),
+        .stmtjrnl_spill => |p| sqlite3_config(option, p),
+        .sorterref_size => |p| sqlite3_config(option, p),
+        .memdb_maxsize => |p| sqlite3_config(option, p),
+    };
+    std.debug.assert((try err.checkSqliteErr(result)) == .Ok);
+}
+
+pub const ConfigParams = union(ConfigOption) {
+    singlethread: void,
+    multithread: void,
+    serialized: void,
+    malloc: *sqlite3_mem_methods,
+    getmalloc: *sqlite3_mem_methods,
+    small_malloc: bool,
+    memstatus: bool,
+    /// Define the memory pool that SQLite can use with the default database page cache implementation.
+    pagecache: struct {
+        /// Pointer to the start of the page cache
+        pMem: *align(8) u8,
+        /// The size of each cache line. This should be a size that is large enough to
+        /// contain the largest database page plus some extra bytes for the page header.
+        sz: c_int,
+        /// The number of caches lines
+        n: c_int,
+    },
+    heap: struct {
+        /// Memory reserved for the heap
+        mem: ?[]align(8) u8,
+        /// The minimum allocation size. The minimum allocation size is capped at 2**12.
+        /// Reasonable values for the minimum allocation size are 2**5 through 2**8.
+        minimumAllocationSize: usize,
+    },
+    mutex: *Mutex.Methods,
+    getmutex: *Mutex.Methods,
+    lookaside: struct {
+        defaultSlotSize: c_int,
+        defaultSlotsPerConnection: c_int,
+    },
+    pcache2: *PCache.Methods2,
+    getpcache2: *PCache.Methods2,
+    log: struct {
+        logFn: fn (userdata: *c_void, errcode: c_int, msg: ?[*]const u8) callconv(.C) void,
+        userdata: ?*c_void,
+    },
+    uri: bool,
+    covering_index_scan: bool,
+    sqllog: struct {
+        logFn: fn (userdata: *c_void, db: *SQLite3, dbFilename: ?[*:0]const u8, sqllog: c_int) callconv(.C) void,
+        userdata: ?*c_void,
+    },
+    mmap_size: struct {
+        defaultSizeLimit: i64,
+        maximumSizeLimit: i64,
+    },
+    win32_heapsize: i32,
+    pcache_hdrsz: *c_int,
+    pmasz: c_uint,
+    stmtjrnl_spill: c_int,
+    sorterref_size: c_int,
+    memdb_maxsize: i64,
+};
+
+pub const ConfigOption = enum(c_int) {
+    singlethread = SQLITE_CONFIG_SINGLETHREAD,
+    multithread = SQLITE_CONFIG_MULTITHREAD,
+    serialized = SQLITE_CONFIG_SERIALIZED,
+    malloc = SQLITE_CONFIG_MALLOC,
+    getmalloc = SQLITE_CONFIG_GETMALLOC,
+    pagecache = SQLITE_CONFIG_PAGECACHE,
+    heap = SQLITE_CONFIG_HEAP,
+    memstatus = SQLITE_CONFIG_MEMSTATUS,
+    mutex = SQLITE_CONFIG_MUTEX,
+    getmutex = SQLITE_CONFIG_GETMUTEX,
+    lookaside = SQLITE_CONFIG_LOOKASIDE,
+    log = SQLITE_CONFIG_LOG,
+    uri = SQLITE_CONFIG_URI,
+    pcache2 = SQLITE_CONFIG_PCACHE2,
+    getpcache2 = SQLITE_CONFIG_GETPCACHE2,
+    covering_index_scan = SQLITE_CONFIG_COVERING_INDEX_SCAN,
+    sqllog = SQLITE_CONFIG_SQLLOG,
+    mmap_size = SQLITE_CONFIG_MMAP_SIZE,
+    win32_heapsize = SQLITE_CONFIG_WIN32_HEAPSIZE,
+    pcache_hdrsz = SQLITE_CONFIG_PCACHE_HDRSZ,
+    pmasz = SQLITE_CONFIG_PMASZ,
+    stmtjrnl_spill = SQLITE_CONFIG_STMTJRNL_SPILL,
+    small_malloc = SQLITE_CONFIG_SMALL_MALLOC,
+    sorterref_size = SQLITE_CONFIG_SORTERREF_SIZE,
+    memdb_maxsize = SQLITE_CONFIG_MEMDB_MAXSIZE,
+};
+
+pub const SQLITE_CONFIG_SINGLETHREAD = @as(c_int, 1);
+pub const SQLITE_CONFIG_MULTITHREAD = @as(c_int, 2);
+pub const SQLITE_CONFIG_SERIALIZED = @as(c_int, 3);
+pub const SQLITE_CONFIG_MALLOC = @as(c_int, 4);
+pub const SQLITE_CONFIG_GETMALLOC = @as(c_int, 5);
+pub const SQLITE_CONFIG_SCRATCH = @as(c_int, 6);
+pub const SQLITE_CONFIG_PAGECACHE = @as(c_int, 7);
+pub const SQLITE_CONFIG_HEAP = @as(c_int, 8);
+pub const SQLITE_CONFIG_MEMSTATUS = @as(c_int, 9);
+pub const SQLITE_CONFIG_MUTEX = @as(c_int, 10);
+pub const SQLITE_CONFIG_GETMUTEX = @as(c_int, 11);
+pub const SQLITE_CONFIG_LOOKASIDE = @as(c_int, 13);
+pub const SQLITE_CONFIG_PCACHE = @as(c_int, 14);
+pub const SQLITE_CONFIG_GETPCACHE = @as(c_int, 15);
+pub const SQLITE_CONFIG_LOG = @as(c_int, 16);
+pub const SQLITE_CONFIG_URI = @as(c_int, 17);
+pub const SQLITE_CONFIG_PCACHE2 = @as(c_int, 18);
+pub const SQLITE_CONFIG_GETPCACHE2 = @as(c_int, 19);
+pub const SQLITE_CONFIG_COVERING_INDEX_SCAN = @as(c_int, 20);
+pub const SQLITE_CONFIG_SQLLOG = @as(c_int, 21);
+pub const SQLITE_CONFIG_MMAP_SIZE = @as(c_int, 22);
+pub const SQLITE_CONFIG_WIN32_HEAPSIZE = @as(c_int, 23);
+pub const SQLITE_CONFIG_PCACHE_HDRSZ = @as(c_int, 24);
+pub const SQLITE_CONFIG_PMASZ = @as(c_int, 25);
+pub const SQLITE_CONFIG_STMTJRNL_SPILL = @as(c_int, 26);
+pub const SQLITE_CONFIG_SMALL_MALLOC = @as(c_int, 27);
+pub const SQLITE_CONFIG_SORTERREF_SIZE = @as(c_int, 28);
+pub const SQLITE_CONFIG_MEMDB_MAXSIZE = @as(c_int, 29);
+
+pub const sqlite3_mem_methods = extern struct {
+    xMalloc: ?fn (c_int) callconv(.C) ?*c_void,
+    xFree: ?fn (?*c_void) callconv(.C) void,
+    xRealloc: ?fn (?*c_void, c_int) callconv(.C) ?*c_void,
+    xSize: ?fn (?*c_void) callconv(.C) c_int,
+    xRoundup: ?fn (c_int) callconv(.C) c_int,
+    xInit: ?fn (?*c_void) callconv(.C) c_int,
+    xShutdown: ?fn (?*c_void) callconv(.C) void,
+    pAppData: ?*c_void,
+};
+
+// Mutex
+pub const Mutex = opaque {
+    pub const Methods = extern struct {
+        xMutexInit: ?fn () callconv(.C) c_int,
+        xMutexEnd: ?fn () callconv(.C) c_int,
+        xMutexAlloc: ?fn (c_int) callconv(.C) ?*Mutex,
+        xMutexFree: ?fn (?*Mutex) callconv(.C) void,
+        xMutexEnter: ?fn (?*Mutex) callconv(.C) void,
+        xMutexTry: ?fn (?*Mutex) callconv(.C) c_int,
+        xMutexLeave: ?fn (?*Mutex) callconv(.C) void,
+        xMutexHeld: ?fn (?*Mutex) callconv(.C) c_int,
+        xMutexNotheld: ?fn (?*Mutex) callconv(.C) c_int,
+    };
+};
+
+// PCache
+pub const PCache = opaque {
+    pub const Page = extern struct {
+        pBuf: ?*c_void,
+        pExtra: ?*c_void,
+    };
+
+    pub const Methods2 = extern struct {
+        iVersion: c_int,
+        pArg: ?*c_void,
+        xInit: ?fn (?*c_void) callconv(.C) c_int,
+        xShutdown: ?fn (?*c_void) callconv(.C) void,
+        xCreate: ?fn (c_int, c_int, c_int) callconv(.C) ?*PCache,
+        xCachesize: ?fn (?*PCache, c_int) callconv(.C) void,
+        xPagecount: ?fn (?*PCache) callconv(.C) c_int,
+        xFetch: ?fn (?*PCache, c_uint, c_int) callconv(.C) [*c]Page,
+        xUnpin: ?fn (?*PCache, [*c]Page, c_int) callconv(.C) void,
+        xRekey: ?fn (?*PCache, [*c]Page, c_uint, c_uint) callconv(.C) void,
+        xTruncate: ?fn (?*PCache, c_uint) callconv(.C) void,
+        xDestroy: ?fn (?*PCache) callconv(.C) void,
+        xShrink: ?fn (?*PCache) callconv(.C) void,
+    };
+
+    pub const Methods = extern struct {
+        pArg: ?*c_void,
+        xInit: ?fn (?*c_void) callconv(.C) c_int,
+        xShutdown: ?fn (?*c_void) callconv(.C) void,
+        xCreate: ?fn (c_int, c_int) callconv(.C) ?*PCache,
+        xCachesize: ?fn (?*PCache, c_int) callconv(.C) void,
+        xPagecount: ?fn (?*PCache) callconv(.C) c_int,
+        xFetch: ?fn (?*PCache, c_uint, c_int) callconv(.C) ?*c_void,
+        xUnpin: ?fn (?*PCache, ?*c_void, c_int) callconv(.C) void,
+        xRekey: ?fn (?*PCache, ?*c_void, c_uint, c_uint) callconv(.C) void,
+        xTruncate: ?fn (?*PCache, c_uint) callconv(.C) void,
+        xDestroy: ?fn (?*PCache) callconv(.C) void,
+    };
+};
+
 // Constants
 pub const SQLITE_OPEN_READONLY = @as(c_int, 0x00000001);
 pub const SQLITE_OPEN_READWRITE = @as(c_int, 0x00000002);
